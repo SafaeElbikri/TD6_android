26bd78a489ae14ef25636d007fe667fb65a46b2b
elbik <elbikrisafae@gmail.com>
2021-11-29 15:02:47 +0100

    first commit

---
+++ app/src/main/java/com/example/td6/ListRepoActivity.java
@@ -0,0 +1,14 @@
+package com.example.td6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListRepoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_repo);
+    }
+}
